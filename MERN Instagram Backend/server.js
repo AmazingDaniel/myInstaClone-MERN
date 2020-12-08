6092bfef277e3732663fd758ccb61fc90490239b
Daniel Moreno <drmjr417@sbcglobal.net>
2020-12-07 21:40:00 -0600

    add dependencies in the package.json last push now creating the server structure

---
+++ MERN Instagram Backend/server.js
@@ -0,0 +1,14 @@
+import cors from 'cors';
+import express from 'express';
+import mongoose from 'mongoose';
+import Pusher from 'pusher';
+
+// app config
+
+// middlewares
+
+// DB config
+
+// api routes
+
+// listen
